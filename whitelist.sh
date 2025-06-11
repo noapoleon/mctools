64bf0555f2bfdc9daf9b66909d90e89eb04e5095
Noapoleon <noalegrand088@gmail.com>
2025-06-11 13:10:52 +0200

    env testing in whitelist.sh

--- whitelist.sh
+++ whitelist.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-print coucou
+# Get the directory where the script lives
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# Define .env path next to the script
+ENV_FILE="$SCRIPT_DIR/.env"
+# Load .env file
+if [[ -f .env ]]; then
+	set -a
+	source $ENV_FILE
+	set +a
+else
+	printf "Error: Required .env file not found in $SCRIPT_DIR\n"
+	exit 1
+fi
+
+printf "Server directory: %s\n"  $SERVER_DIR
+printf "RCON Host: %s\n"  $RCON_HOST
+printf "RCON Port: %s\n"  $RCON_PORT
+printf "RCON Pass: %s\n"  $RCON_PASS
